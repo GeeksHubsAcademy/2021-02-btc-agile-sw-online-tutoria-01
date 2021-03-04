300d1fb8ce721edbf49b005ca78bc9ff1a77a982
vicboma1 <victorbolinchesmarin@gmail.com>
2021-03-04 18:56:07 +0100

    test passed 5/5

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,14 +1,16 @@
+
 export function fizzBuzz(input : number) : string {
     var res = "";
 
     for(var i = 1; i <= input; i++ ){
-        if(i%3 == 0 )
+        if (i%3 == 0 && i%5 == 0)
+            res += "FizzBuzz"+"\n"
+        else if(i%3 == 0)
             res += "Fizz"+"\n";
-        else if(i%5 == 0 )
+        else if(i%5 == 0)
             res += "Buzz"+"\n"
         else 
             res += i+"\n";
-
     }
 
     return res;
