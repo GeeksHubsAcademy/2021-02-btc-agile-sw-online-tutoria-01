d80d095869aee3ce66c2f470a4237c652edba76a
vicboma1 <victorbolinchesmarin@gmail.com>
2021-03-04 18:59:15 +0100

    new refactor method

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,14 +1,18 @@
+const ENTER = "\n";
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+const FIZBUZZ = FIZZ + BUZZ;
 
 export function fizzBuzz(input : number) : string {
     var res = "";
 
     for(var i = 1; i <= input; i++ ){
         if (i%3 == 0 && i%5 == 0)
-            res += "FizzBuzz"+"\n"
+            res += FIZBUZZ + ENTER;
         else if(i%3 == 0)
-            res += "Fizz"+"\n";
+            res += FIZZ + ENTER;
         else if(i%5 == 0)
-            res += "Buzz"+"\n"
+            res += BUZZ + ENTER
         else 
             res += i+"\n";
     }
