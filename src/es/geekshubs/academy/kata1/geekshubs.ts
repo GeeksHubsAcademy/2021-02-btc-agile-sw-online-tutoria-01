a5ff567fa77317b3252f5ec16d3771fb044e7345
vicboma1 <victorbolinchesmarin@gmail.com>
2021-03-04 18:48:51 +0100

    test passed 1/5

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,3 +1,11 @@
 export function fizzBuzz(input : number) : string {
-    return ""
+    var res = "";
+
+    for(var i = 1; i <= input; i++ ){
+
+            res += i+"\n";
+
+    }
+
+    return res;
 };
