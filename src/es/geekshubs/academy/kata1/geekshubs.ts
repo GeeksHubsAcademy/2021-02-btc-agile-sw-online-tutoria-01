7a529e4ad77283d7fa9d2c7a8e9e3e2f77f1326a
vicboma1 <victorbolinchesmarin@gmail.com>
2021-03-11 18:01:30 +0100

    append new functions

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,21 +1,35 @@
+const EMPTY = "";
 const ENTER = "\n";
 const FIZZ = "Fizz";
 const BUZZ = "Buzz";
 const FIZBUZZ = FIZZ + BUZZ;
 
-export function fizzBuzz(input : number) : string {
-    var res = "";
+export function fizzBuzz(input : Number) : String {
+    var res = EMPTY;
 
-    for(var i = 1; i <= input; i++ ){
-        if (i%3 == 0 && i%5 == 0)
+    for(var i = 1; i <= input; i++ ) {
+        if (isMod_3_or_5(i))
             res += FIZBUZZ + ENTER;
-        else if(i%3 == 0)
+        else if(isMod_3(i))
             res += FIZZ + ENTER;
-        else if(i%5 == 0)
+        else if(isMod_5(i))
             res += BUZZ + ENTER
         else 
-            res += i+"\n";
+            res += i + ENTER;
     }
 
     return res;
 };
+
+function isMod_5(i: number): boolean {
+    return i % 5 == 0;
+}
+
+function isMod_3(i: number) {
+    return i % 3 == 0;
+}
+
+function isMod_3_or_5(i: number) {
+    return isMod_3(i) && isMod_5(i);
+}
+
